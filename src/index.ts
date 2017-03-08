b3fdc0abe71bbc04c92ad2e56341dcccc366e5ce
Mikko <k8867@student.jamk.fi>
2017-03-08 18:20:13 +0200

    Loader added.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
-
 import { InitCanvas } from './InitCanvas';
 import { Circle } from './Circle';
 import { Text } from './Text';
+import * as px from './PxLoader';
+
+class Preload {
+    constructor() {
+        var baseUrl: string = 'http://thinkpixellab.com/pxloader' +
+            '/slowImage.php?delay=1&time=' + new Date;
+
+        var loader: px.PxLoader = new px.PxLoader();
+
+        for (var i: number = 0; i < 100; i++) {
+            var pxImage: px.PxLoaderImage = new px.PxLoaderImage(baseUrl + '&i=' + i);
+            loader.add(pxImage);
+
+            loader.addProgressListener(this.onProgress);
+            loader.addCompletionListener(this.onComplete);
+        }
+    }
+
+    onProgress = (e: any): void => {
+        console.log("items loaded: " + (e.completedCount / e.totalCount));
+    }
+
+    onComplete = (e: any): void => {
+        console.log("Loading completed.");
+    }
+
+}
+
+const myPreload: Preload = new Preload();
+myPreload.onProgress;
+myPreload.onComplete;
+
 
 const myCanvas: InitCanvas = new InitCanvas('animationCanvas', 640, 480);
 const canvasHeight = myCanvas.getCanvasHeight();
 const canvasWidth = myCanvas.getCanvasWidth();
 
 const text: Text = new Text(canvasWidth, 120, 'orange');
+
+
 
 const circles = new Array();
 for (var i = 0; i < 25; i += 1) {
@@ -35,3 +68,5 @@ window.onload = (): void => {
     myCanvas.createCanvas();
     draw();
 }
+
+
