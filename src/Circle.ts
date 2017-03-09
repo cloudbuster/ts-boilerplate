0c19a7f483826286aba19248fcbadd654fbf41e5
Mikko <k8867@student.jamk.fi>
2017-03-09 21:27:14 +0200

    Looping scroller

--- src/Circle.ts
+++ src/Circle.ts
@@ -8,13 +8,13 @@ export class Circle implements Shape {
     radius: number;
     lineWidth: number;
     color: string;
+    counter: number = 0;
 
     constructor(x: number, y: number, radius: number, color: string) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
-        var i = 100;
     }
 
     randomInit(canvasHeight: number, canvasWidth: number, layers: number): void {
@@ -26,10 +26,23 @@ export class Circle implements Shape {
         this.radius = (this.xSpeed / 3);
     }
 
+
     update(canvasHeight: number, canvasWidth: number): void {
         const min: number = 0;
         this.x += this.xSpeed;
         if (this.x > canvasWidth) this.x = -1 * (this.radius);
+    }
+
+    anotherUpdate(direction: number): void {
+        this.counter += 1;
+        this.x += Math.round(direction);
+        if (this.counter > (this.radius*2)-1 && direction <= -1) {
+            this.x += this.radius*2;
+            this.counter = 0;
+        } else if (this.counter > (this.radius*2)-1 && direction >= 1) {
+            this.x -= this.radius*2;
+            this.counter = 0;
+        }
     }
 
 
@@ -42,6 +55,17 @@ export class Circle implements Shape {
         context.stroke();
         context.restore();
     }
+
+    fill(context: CanvasRenderingContext2D): void {
+        context.save();
+        context.beginPath();
+        context.fillStyle = this.color;
+        context.lineWidth = this.lineWidth;
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.fill();
+        context.restore();
+    }
+
 
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
-import { InitCanvas } from './InitCanvas';
+import {InitCanvas} from "./InitCanvas";
+import {Circle} from "./Circle";
 import { Text } from './Text';
 
-console.log(100);
 
 const myCanvas: InitCanvas = new InitCanvas('animationCanvas', 640, 480);
 const canvasHeight = myCanvas.getCanvasHeight();
 const canvasWidth = myCanvas.getCanvasWidth();
+const radius: number = 20   ;
+const text: Text = new Text(0, 0, 'white');
 
-const text: Text = new Text(canvasWidth, 120, 'orange');
+
+const circles = new Array();
+for (var i = 0; i < 85; i += 1) {
+    circles.push(new Circle((i*(radius*2))-(radius*4), radius, radius, 'orange'));
+}
+
+
+const circlesTwo = new Array();
+for (var i = 0; i < 85; i += 1) {
+    circlesTwo.push(new Circle((i*(radius*2))-(radius*4), radius*4, radius, 'yellow'));
+}
+
+// const text: Text = new Text(canvasWidth, 120, 'orange');
 
 var draw = (): void => {
     myCanvas.context.fillStyle = '#222222';
-    myCanvas.context.fillRect(0, 0, canvasWidth, canvasHeight);
+    myCanvas.context.clearRect(0, 0, canvasWidth, canvasHeight);
 
     requestAnimationFrame(draw);
+
+    for (var i in circles) {
+        circles[i].anotherUpdate(1);
+        circles[i].fill(myCanvas.context);
+    }
+
+    for (var i in circles) {
+        circlesTwo[i].anotherUpdate(1);
+        circlesTwo[i].fill(myCanvas.context);
+    }
 
     text.update(canvasHeight, canvasWidth);
     text.draw(myCanvas.context)
